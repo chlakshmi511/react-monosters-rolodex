0c384134674d5a8a2f56f9e0337d1c30a4025e7f
c-cognizant-lchilaka <lakshmi.chilakamarti@turner.com>
2021-03-31 17:56:27 +0530

    [adding files to github pages]

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -27,6 +27,7 @@ class App extends Component{
       const filteredMonsters = monsters.filter( monster => monster.name.toLowerCase().includes( searchField.toLowerCase()));
      return (
       <div className='App'>
+        <h1>Monosters Rolodex</h1>
         <SearchBox placeholder='search monsters' handleChange={this.handleChange}></SearchBox>
        
         <CardList monsters= {filteredMonsters}></CardList>
